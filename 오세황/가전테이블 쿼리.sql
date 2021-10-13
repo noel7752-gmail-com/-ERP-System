77eff8c9067253a073c0bd06c4b0de9a09df5477
spada9403 <osh9403@gmail.com>
2021-10-13 21:50:11 +0900

    Gajun table update
    가전 테이블 오라클 쿼리 밑 RED 업데이트

--- 오세황/가전테이블 쿼리.sql
+++ 오세황/가전테이블 쿼리.sql
@@ -6,10 +6,10 @@ color_name varchar2(20) not null unique,
 primary key(color_code)
 );
 /*에너지소비등급 테이블*/
-create table MEPS(
-MEPS_code varchar2(20),
-MEPS_rating varchar2(20) not null unique,
-primary key(MEPS_code)
+create table energy_grade(
+energy_grade_code varchar2(20),
+energy_grade_name varchar2(20) not null unique,
+primary key(energy_grade_code)
 );
 /*브랜드 테이블*/
 create table brand(
@@ -17,58 +17,55 @@ brand_code varchar2(20),
 brand_name varchar2(20) not null unique,
 primary key(brand_code)
 );
-/*가전 소분류 테이블*/
-CREATE TABLE BO_ID_gajun_semicategory(
-    gajun_semicategory_code varchar2(20),
-    gajun_semicategory_name VARCHAR2(20) NOT NULL UNIQUE,
-    PRIMARY KEY(gajun_semicategory_code)
-);
-/* itemsTable */
+/* 가전/TV/모바일/PC */
 create table BO_ID_categories(
     category_code varchar2(20),
     category_name varchar2(20) not null,
     primary key(category_code)
 );
 /*가전 대분류 테이블*/
-create table BO_ID_gajun_category(
+create table BO_ID_Gcategory(
     category_code,
-    gajun_category_code varchar2(20),
-    gajun_category_name VARCHAR2(20) NOT NULL UNIQUE,
-    gajun_semicategory1,
-    gajun_semicategory2,
-    gajun_semicategory3,
-    PRIMARY KEY(gajun_category_code),
-    foreign key(gajun_semicategory1) references BO_ID_gajun_semicategory(gajun_semicategory_code),
-    foreign key(gajun_semicategory2) references BO_ID_gajun_semicategory(gajun_semicategory_code),
-    foreign key(gajun_semicategory3) references BO_ID_gajun_semicategory(gajun_semicategory_code),
+    Gcategory_code varchar2(20),
+    Gcategory_name VARCHAR2(20) NOT NULL UNIQUE,
+    PRIMARY KEY(Gcategory_code),
     foreign key(category_code) references BO_ID_categories(category_code)
 );
 /*제품 테이블*/
-create table BO_ID_gajun_items(
-   pic varchar2(200),
-   gajun_category_code,
-   gajun_semicategory,
+create table BO_ID_Gitems(
+   thumbnail varchar2(200),
+   Gcategory_code,
+   category_code,
    brand_code,
-   item_code varchar2(20),
-   item_name varchar2(20) not null,
-   input_day date default sysdate,
-   output_day date,
+   Gitem_code varchar2(20),
+   Gitem_name varchar2(20) not null,
    build_day date,
-   MEPS_code,
+   energy_grade_code,
    power_consum varchar2(20) not null,
    color_code,
-   stock number(4) default 0,
    whatsize varchar(2),
    discontinued varchar2(10) default 'false',
-   primary key(item_code),
-   foreign key(gajun_semicategory) references BO_ID_gajun_semicategory(gajun_semicategory_code),
+   primary key(Gitem_code),
+   foreign key(Gcategory_code) references BO_ID_Gcategory(Gcategory_code),
+   foreign key(category_code) references BO_ID_categories(category_code),
    foreign key(brand_code) references brand(brand_code),
-   foreign key(MEPS_code) references MEPS(MEPS_code),
-   foreign key(color_code) references color(color_code),
-   foreign key(gajun_category_code) references BO_ID_gajun_category(gajun_category_code)
+   foreign key(energy_grade_code) references energy_grade(energy_grade_code),
+   foreign key(color_code) references color(color_code)
 );
-
-
+/* 입고일 날짜 저장 테이블 */
+create table input_date (
+    Gitem_code,
+    input_date date not null,
+    stock_in_cnt number(4) not null,
+    foreign key(Gitem_code) references BO_ID_Gitems(Gitem_code)
+);
+/* 출고일 날짜 저장 테이블 */
+create table output_date (
+    Gitem_code,
+    output_date date not null,
+    stock_out_cnt number(4) not null,
+    foreign key(Gitem_code) references BO_ID_Gitems(Gitem_code)
+);
 
 drop table BO_ID_brand;
 drop table BO_ID_MEPS;
