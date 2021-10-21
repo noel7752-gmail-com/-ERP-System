d767687ca3a02ebb6b242751d21fa537625f3b04
spada9403 <osh9403@gmail.com>
2021-10-21 16:35:19 +0900

    Create 재고수량_단종여부 쿼리-2021.10.21.sql

---
+++ 오세황/통합검색쿼리/재고수량_단종여부 쿼리-2021.10.21.sql
@@ -0,0 +1,25 @@
+/* 재고수량이 0인 애들 & 단종여부 */
+
+/* where 절에 조건 추가. <-- 서버에 부담이 많음 */
+select
+items.*
+from
+ID_G_ITEMS items
+where
+items.G_DISCONTINUED = 'false'
+or
+((select nvl(sum(G_STOCK_IN_CNT),0) from ID_G_INPUT_DATE indate where indate.G_ITEM_CODE = G_ITEM_CODE)-(select nvl(sum(G_STOCK_OUT_CNT),0) from ID_G_OUTPUT_DATE outdate where outdate.G_ITEM_CODE = G_ITEM_CODE)) = 1
+
+
+
+/*ansi 방식 inner join 으로 테이블을 추가 서버에 부담이 적음 */
+/* inday 테이블과 outday 테이블의 CNT 빼면 남은 재고수량*/
+select items.*, inday.*, outday.* from
+ID_G_ITEMS items inner join
+(select nvl(sum(G_STOCK_IN_CNT),0), G_ITEM_CODE from ID_G_INPUT_DATE group by G_ITEM_CODE) inday on inday.G_ITEM_CODE = items.G_ITEM_CODE inner join
+(select nvl(sum(G_STOCK_OUT_CNT),0), G_ITEM_CODE from ID_G_OUTPUT_DATE group by G_ITEM_CODE) outday on outday.G_ITEM_CODE = items.G_ITEM_CODE
+where
+items.G_DISCONTINUED = 'true'
+
+
+
