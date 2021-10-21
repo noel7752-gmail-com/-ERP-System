a95c7b6374f48da4556a232505eedb53ed567421
spada9403 <osh9403@gmail.com>
2021-10-21 18:12:34 +0900

    Update 품목별 브랜드_컬러 가져오는 쿼리 2021.10.21.sql

--- 오세황/통합검색쿼리/품목별 브랜드_컬러 가져오는 쿼리 2021.10.21.sql
+++ 오세황/통합검색쿼리/품목별 브랜드_컬러 가져오는 쿼리 2021.10.21.sql
@@ -7,7 +7,10 @@ select * from ID_G_SUB_CATEGORY where CATEGORY_CODE = 1
 select * from ID_G_SUB_SUB_CATEGORY where G_SUB_CATEGORY_CODE = 1
 
 /* 각각 카테고리별로 가지고 있는 브랜드 / 컬러 가져오는 쿼리                       ▽ 여기에 추가 조건(카테고리)이 들어가면 됩니다.*/
-select * from brand where brand_code in (select brand_code from ID_G_ITEMS where category_code = 1 group by(brand_code))
-select * from color where color_code in (select color_code from ID_G_ITEMS where category_code = 1 group by(color_code))
+select * from brand where brand_code in (select distinct(brand_code) from ID_G_ITEMS where category_code = 1)
+select * from color where color_code in (select distinct(color_code) from ID_G_ITEMS where category_code = 1 )
+
+select * from
 
 
+commit
