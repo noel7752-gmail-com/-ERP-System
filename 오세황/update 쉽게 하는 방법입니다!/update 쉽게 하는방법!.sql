9a1a42fd69f5c1bb3571e6952fee620cbd190079
spada9403 <osh9403@gmail.com>
2021-10-25 22:01:26 +0900

    Update update 쉽게 하는방법!.sql

--- 오세황/update 쉽게 하는 방법입니다!/update 쉽게 하는방법!.sql
+++ 오세황/update 쉽게 하는 방법입니다!/update 쉽게 하는방법!.sql
@@ -18,7 +18,7 @@ end;
 
     저도 자세한것은 모르고 복붙해왔습니다.
     ITEM_CODE_UPDATE 라는 이름으로 trigger를 생성,
-    이 트리거는 ID_GI_ITEMS 테이블에 G_ITEM_CODE 가 update 된 후 실행됩니다.
+    이 트리거는 ID_G_ITEMS 테이블에 G_ITEM_CODE 가 update 된 후 실행됩니다.
     시작
         만약 input_date 테이블 제품코드가 예전거라면 방금 바뀐 새 값을 넣어줌.
         만약 output_date 테이블 제품코드가 예전거라면 방금 바뀐 새 값을 넣어줌.
